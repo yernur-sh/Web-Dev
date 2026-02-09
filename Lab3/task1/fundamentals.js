4d03ee2f24887b76593cdf47392fb640e9a5244c
yernur-sh <ernurshahar0@gmail.com>
2026-02-09 21:13:35 +0500

    Lab3

---
+++ Lab3/task1/fundamentals.js
@@ -0,0 +1,16 @@
+
+let admin, name;
+name = "John";
+admin = name;
+
+let message = "hello";
+message = 123456;
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed ${str}`; 
+
+let user = {
+    name: "John",
+    age: 30
+};
