7e27fcb14a9edb6cb5787006a86c4b6f1bb320b2
yernur-sh <ernurshahar0@gmail.com>
2026-02-10 21:11:52 +0500

    Lab3

--- Lab3/task2/script.js
+++ Lab3/task2/script.js
@@ -6,23 +6,23 @@ function addTask() {
     const text = input.value.trim();
     if (text === "") return;
 
-    // 1. Жаңа элемент жасау
+
     const li = document.createElement('li');
     li.className = 'todo-item';
 
-    // 2. Чекбокс жасау
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.onclick = function() {
         span.classList.toggle('done', checkbox.checked);
     };
 
-    // 3. Мәтін жасау
+
     const span = document.createElement('span');
     span.className = 'todo-text';
     span.textContent = text;
 
-    // 4. Өшіру батырмасы (trash icon)
+
     const delBtn = document.createElement('button');
     delBtn.innerHTML = '🗑';
     delBtn.className = 'delete-btn';
@@ -30,18 +30,18 @@ function addTask() {
         li.remove();
     };
 
-    // 5. Тізімге қосу
+
     li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(delBtn);
     todoList.appendChild(li);
 
-    input.value = ""; // Енгізу өрісін тазалау
+    input.value = "";
 }
 
 addBtn.addEventListener('click', addTask);
 
-// Enter батырмасын басуды қадағалау
+
 input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') addTask();
 });
