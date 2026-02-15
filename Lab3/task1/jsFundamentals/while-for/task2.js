07156fce1c0954f47c03a781aa21c91426b5593e
yernur-sh <ernurshahar0@gmail.com>
2026-02-15 21:48:35 +0500

    Lab3

--- Lab3/task1/jsFundamentals/while-for/task2.js
+++ Lab3/task1/jsFundamentals/while-for/task2.js
@@ -2,6 +2,6 @@ let i = 0;
 while (++i < 5) alert( i );
 //shows from 1 to 4
 
-let i = 0;
+let ij = 0;
 while (i++ < 5) alert( i );
 //shows from 0 to 4
--- Lab3/task1/jsFundamentals/while-for/task4.js
+++
@@ -1,3 +0,0 @@
-for(int i = 2; i <= 10; i+=2){
-    alert(i);
-}
